76845e4a3170cc65da18fedda2595492f7e0f92a
MICHAEL HAZANI <123mickey@gmail.com>
2015-07-27 13:20:54 -0700

    add lights + rotation add lever sound, logic

---
+++ Assets/turnOn.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class turnOn : MonoBehaviour {
+public GameObject lightParent;
+	private AudioSource audio;
+	private bool isLight;
+	// Use this for initialization
+	void Start () {
+	audio = GameObject.FindWithTag("lever").GetComponent<AudioSource>();
+	isLight = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Test")){
+			if (!isLight) {
+				isLight = true;
+				var parentLight = GameObject.FindWithTag("allLights");
+				parentLight.GetComponent<RotateLight>().enabled = true;
+
+				var lights = lightParent.GetComponentsInChildren<Light>(true);
+				
+				foreach (Light light in lights)
+				{
+					light.enabled = true;
+				}
+				audio.Play();
+
+			} else {
+
+				isLight = false;
+				var parentLight = GameObject.FindWithTag("allLights");
+				parentLight.GetComponent<RotateLight>().enabled = false;
+				
+				var lights = lightParent.GetComponentsInChildren<Light>(true);
+				
+				foreach (Light light in lights)
+				{
+					light.enabled = false;
+				}
+				audio.Play();
+			}
+		}
+	}
+}
+
