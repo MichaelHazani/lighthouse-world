b3e507ce256074dd6010d5ce3ed5364763bc85b4
MICHAEL HAZANI <123mickey@gmail.com>
2015-07-28 22:03:14 -0700

    add nocturne

---
+++ Assets/playNocturneAtNight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playNocturneAtNight : MonoBehaviour {
+	private AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		int time = System.Int32.Parse(System.DateTime.Now.ToString("%H%mm"));
+		Debug.Log (time);
+		if (time > 2000) {
+			audio.Play();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
